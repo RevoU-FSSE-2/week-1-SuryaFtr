f670bf5baeeba8346ef59548ef9a4d5a8207b6e5
Surya Faturohman <suryafaturohman@gmail.com>
2023-06-16 19:56:30 +0700

    Edit web style html css javascript include some fontawesome icon

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Personal Website
 Website ini hanya merupakan sebuah website pribadi yang dibuat untuk tujuan membuat konten memperkenalkan diri dan menjelaskan siapa pribadi diri sendiri.
 
+![My Photo](/assets/img/img-1.jpg "This is ME")
+
+Surya Faturohman (suryafaturohman@gmail.com)
+RevoU FSSE Section Barcelona
+Team 6
+
 ## Bahasa yang digunakan website ini
 
 1. HTML
@@ -9,7 +15,7 @@ Website ini hanya merupakan sebuah website pribadi yang dibuat untuk tujuan memb
 
 ## Link Website
 
-Link website dengan netlify [Surya Faturohman Personal Website](https://suryaftr-personal.netlify.app).
+Link website dengan netlify [https://suryaftr-personal.netlify.app](https://suryaftr-personal.netlify.app).
 
 
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/l9v8sNrv)
--- assets/js/style.js
+++ assets/js/style.js
@@ -1,3 +1,23 @@
 function clickMe(){
     window.alert("Hello ... My Name is Surya Faturohman");
 }
+
+
+
+document.getElementById("contact").addEventListener("click", showHideContact);
+function showHideContact() {
+    document.getElementById("contact").classList.add("a-active");
+    document.getElementById("home").classList.remove("a-active");
+    document.getElementById("title").innerHTML = '<i class="fa fa-phone-square"></i> Contact';
+    document.getElementById("p1").innerHTML = '<i class="fa fa-mobile-phone"></i> Phone : 0838 1142 2863<br><br><i class="fa fa-envelope-square"></i> E-Mail : suryafaturohman@gmail.com';
+    document.getElementById("p2").innerHTML = '<i class="fa  fa-address-book"></i> Address : Jl. Dekeng No.49 RT02/RW03 Kel.Genteng Kec.Bogor Selatan Kode POS. 16137';
+}
+
+document.getElementById("home").addEventListener("click", showHideHome);
+function showHideHome() {
+    document.getElementById("home").classList.add("a-active");
+    document.getElementById("contact").classList.remove("a-active");    
+    document.getElementById("title").innerHTML = '<i class="fa fa-user"></i> About Me';
+    document.getElementById("p1").innerHTML = '<i class="fa fa-quote-left fa-2x fa-pull-left fa-border"></i>I am from Bogor and the graduated student of a high school majoring in software engineering class on 2015 and also a graduate of Pakuan University majoring in computer science on 2022.';
+    document.getElementById("p2").innerHTML = '<i class="fa fa-quote-left fa-2x fa-pull-left fa-border"></i>My motivation to join RevoU was to return to my former soul and enthusiasm, to code a program and upgrade my skills more in the field of software engineering.';
+}
